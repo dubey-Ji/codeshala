445021581c14d6666f62b63727462a5c53a40022
RAJPUT JI <vivekrajputthakur09@gmail.com>
2026-06-06 08:22:21 +0530

    first task by vivek

---
+++ python/vivek_lecture1exercise.py
@@ -0,0 +1,33 @@
+#TASK1 - VARIABLE
+
+name = 'Vivek'
+age = 19
+live_at = 'gola ka mandir, Gwalior'
+print (f"My name is {name}. My age is {age}. I live at {live_at}. ")
+
+
+#TASK2 - OPERATORS
+
+first_number = 5
+second_number = 4
+print (first_number + second_number)
+print (first_number - second_number)
+print (first_number * second_number)
+print (first_number / second_number)
+print (first_number % second_number)
+
+#TASK3 - TYPE CHECKING
+
+P = True
+X = 10
+Y = 20.5
+Z = "Hello, World!"
+print (type(X))
+print (type(Y)) 
+print (type(Z))
+print (type(P))
+print (type(first_number))
+print (type(second_number)) 
+print (type(name))
+print (type(age))
+print (type(live_at))
