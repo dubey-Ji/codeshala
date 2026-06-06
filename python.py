a4ba10692733eee4ccfa094e595ff89f6a65036a
Raj Sharma <rajsharma162008@gmail.com>
2026-06-06 12:17:47 +0530

    make new file and do python code

---
+++ python.py
@@ -0,0 +1,27 @@
+# variabl
+#firstname = "Raj"
+#lastname = "sharma"
+#age = 20 
+#city = "gwalior"
+#print(f"this my name {firstname} this my last name {lastname} this is my age {age} and i live in {city}")
+
+# operators 
+
+#print(5+10)
+#print(20-5)
+#print(79/3)
+#print(79//3)
+#print(79%3)
+
+#type check
+
+#rupees = 599
+#sgpa = 71.938576758940
+#name = "raj" 
+#are_you_18 = "true"
+#are_you_not_18 = "false" 
+
+#print(type(rupees))
+#print(type(sgpa))
+#print(type(are_you_18))
+#print(type(are_you_not_18))#
