9b491eb68f8f23d7e19e2caf80a50a72c98345c1
Raj Sharma <rajsharma162008@gmail.com>
2026-06-10 19:56:39 +0530

    temperature convert not done yet

--- python3.py
+++ python3.py
@@ -1,14 +1,25 @@
-num1 = int(input("enter number"))
-num2 = int(input("enter number"))
-operator = input("+,-,*,/")
-def calc(num1,num2,operator):
- if operator == "+":
-  print( num1 + num2)
- elif operator == "-":
-  print("your sub is", num1 - num2)
- elif operator == "*":
-  print("your multiple is", num1 * num2)
- elif operator == "/":
-  print("your divide is", num1/num2)
+#num1 = int(input("enter number"))
+#num2 = int(input("enter number"))
+#operator = input("+,-,*,/")
+#def calc(num1,num2,operator):
+ #if operator == "+":
+  #return( num1 + num2)
+ #elif operator == "-":
+  #return("your sub is", num1 - num2)
+ #elif operator == "*":
+  #return("your multiple is", num1 * num2)
+ #elif operator == "/":
+  #return("your divide is",num1/num2)
+ 
 
- print(calc(num1,num2,operator))
+#print (calc(num1,num2,operator))
+
+
+#celcious to fahrenheit
+
+celcious = int(input("enter celcious value"))
+def celcious_to_fahrenheit(celcious):
+   print(celcious*9/5)+32
+
+while (celcious)
+  
