1c8d9d2295dec07b851138558c56765792e2c427
Raj Sharma <rajsharma162008@gmail.com>
2026-06-11 20:14:20 +0530

    password checker

--- python3.py
+++ python3.py
@@ -1,18 +1,18 @@
-num1 = int(input("enter number : "))
-num2 = int(input("enter number : "))
-operator = input("+,-,*,/ : ")
-def calc(num1,num2,operator):
- if operator == "+":
-  return( num1 + num2)
- elif operator == "-":
-  return("your sub is", num1 - num2)
- elif operator == "*":
-  return("your multiple is", num1 * num2)
- elif operator == "/":
-  return("your divide is",num1/num2)
+#num1 = int(input("enter number : "))
+#num2 = int(input("enter number : "))
+#operator = input("+,-,*,/ : ")
+#def calc(num1,num2,operator):
+ #if operator == "+":
+  #return( num1 + num2)
+ #elif operator == "-":
+  #return("your sub is", num1 - num2)
+ #elif operator == "*":
+  #return("your multiple is", num1 * num2)
+ #elif operator == "/":
+  #return("your divide is",num1/num2)
  
 
-print (calc(num1,num2,operator))
+#print (calc(num1,num2,operator))
 
 
 #no_of_times = int(input("enter loop value : "))
@@ -29,3 +29,15 @@ print (calc(num1,num2,operator))
 
 #password
 
+password = input("enter the password : ")
+
+def is_strong_password(password):
+    return true:
+   if len(password)>8:
+   elif len(password)<8:
+    return false:
+   elif 
+    return false:
+  
+  
+ 
